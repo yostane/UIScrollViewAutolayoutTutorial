8210ecb3ce2c7052f43fd546b542ef8cebf1b2e0
yassine benabbas <yassine.benabbas@gmail.com>
2016-03-28 15:16:16 +0200

    1st commit

---
+++ UIScrollViewAutolayout/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  UIScrollViewAutolayout
+//
+//  Created by yassine benabbas on 27/03/2016.
+//  Copyright © 2016 yostane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
